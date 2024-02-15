a5663573591bfd72db061a7f07f8d20da7f493c5
Pankaj Kumar Das <pdas0354158@gmail.com>
2024-02-16 01:28:46 +0530

    initial

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage(){
+ return(
+    <div>
+    <h1>Hello mr come to login page</h1>
+    </div>
+ )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUp(){
+    return (
+        <div>
+        <h1>Hello mr come to sign up page</h1>
+        </div>
+    )
+}
